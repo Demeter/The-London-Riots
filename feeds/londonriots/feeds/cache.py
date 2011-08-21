eb44d00b80d8c7c3a42f286abdada5e8f7676e44
Evan Cofsky <evan@tunixman.com>
2011-08-21 16:06:35 +0100

    Added "closing" for the caches so that they work as context managers.

--- feeds/londonriots/feeds/cache.py
+++ feeds/londonriots/feeds/cache.py
@@ -2,9 +2,20 @@
 
 from feedcache import Cache as fcache
 import shelve
+from contextlib import closing
 
 CACHE_STORE = ".feedcache"
 ARTICLE_STORE = ".articles"
+
+class Article(object):
+    """Represents a cached article."""
+
+    def __init__(self, entry):
+        self.entry = entry
+
+    def __call__(self):
+        self.content = requests.get(self.entry.link).content
+        return self
 
 class ArticleCache(object):
     """Downloads and caches articles from a feed."""
@@ -12,16 +23,24 @@ class ArticleCache(object):
     def __init__(self, article_store=ARTICLE_STORE):
         self.article_store_path = article_store
 
-    def fetch_article(self, article_store, link):
-        content = requests.get(link).content
-        article_store[link] = content
-        return content
+    def fetch_article(self, article_store, entry):
+        article = Article(entry)
+        article_store[entry.link] = content
+        return article
 
     def __iter__(self, feed_entries):
-        with shelve.open(self.article_store_path) as article_store:
-
+        with closing(shelve.open(self.article_store_path)) as article_store:
             for entry in feed_entries:
                 if entry.link in article_store:
                     continue
 
-                yield self.fetch_article(article_store, entry.link)
+                yield self.fetch_article(article_store, entry)
+
+class FeedCache(object):
+    def __init__(self, url, cache_store=CACHE_STORE):
+        self.url = url
+        self.cache_store_path = cache_store
+
+    def __call__(self):
+        with closing(shelve.open(self.cache_store_path)) as cache_store:
+            return fcache(cache_store).fetch(self.url)
