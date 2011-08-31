b766ce0cbd6379ce91c3a701d9398d8dbf3b2eeb
Evan Cofsky <evan@tunixman.com>
2011-08-31 16:48:18 +0100

    Made londonriots Pylons project the top level, liblondonriots a child.

--- londonriots/README.txt
+++
@@ -1,1 +0,0 @@
-londonriots README
--- setup.py
+++ setup.py
@@ -1,31 +1,54 @@
+import os
+import sys
+
 from setuptools import setup, find_packages
-setup(
-    name = "londonriots",
-    version = "0.1",
-    packages = find_packages(),
 
-    # Project uses reStructuredText, so ensure that the docutils get
-    # installed or upgraded on the target machine
-    install_requires = ["requests",
-                        "feedparser",
-                        "feedcache",
-                        "sqlalchemy",
-                        "pg8000",
-                        "BeautifulSoup",
-                        "nltk"
-                        ],
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-    package_data = {
-        # If any package contains *.txt or *.rst files, include them:
-        "": ["*.txt", "*.rst"],
-    },
+requires = [
+    'pyramid',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'sqlalchemy',
+    'zope.sqlalchemy',
+    'requests',
+    'feedparser',
+    'feedcache',
+    'pg8000',
+    'BeautifulSoup',
+    'nltk'
+    ]
 
-    test_suite = "londonriots",
-    test_loader = "unittest"
-    # metadata for upload to PyPI
-    author = "Evan Cofsky",
-    author_email = "evan@tunixman.com",
-    description = "The London Riots currency trading system.",
-    license = "GPLv3",
-    url = "https://github.com/tunixman/The-London-Riots",
-)
+if sys.version_info[:3] < (2,5,0):
+    requires.append('pysqlite')
+
+setup(name='londonriots',
+      version='0.1',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author = "Evan Cofsky",
+      author_email = "evan@tunixman.com",
+      description = "The London Riots currency trading system.",
+      license = "GPLv3",
+      url = "https://github.com/tunixman/The-London-Riots",
+      keywords='web pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires = requires,
+      tests_require = requires,
+      test_suite="londonriots",
+      entry_points = """\
+      [paste.app_factory]
+      main = londonriots:main
+      """,
+      paster_plugins=['pyramid'],
+      )
+
