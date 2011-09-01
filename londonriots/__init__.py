e882974e8d0d0aa3bdcd64943d1296d9a60fd411
Evan Cofsky <evan@tunixman.com>
2011-09-01 17:35:59 +0100

    Updated to work with a Postgres server listening on an odd port. I think.

--- londonriots/__init__.py
+++ londonriots/__init__.py
@@ -1,5 +1,6 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
+from sqlalchemy.orm import sessionmaker
 
 from londonriots.models import appmaker
 
@@ -7,6 +8,7 @@ def main(global_config, **settings):
     """ This function returns a WSGI application."""
 
     engine = engine_from_config(settings, 'sqlalchemy.')
+    
     get_root = appmaker(engine)
     config = Configurator(settings=settings, root_factory=get_root)
     config.add_static_view('static', 'londonriots:static')
--- londonriots/scripts/pginit.py
+++ londonriots/scripts/pginit.py
@@ -11,7 +11,7 @@ pgdata_path = opath.join(sys.exec_prefix, "pgdata")
 
 os.environ["PATH"] = ":".join((os.environ["PATH"], "/usr/lib/postgresql/8.4/bin"))
 
-postgres_options = "-h '' -k %(pgdata_path)s" % locals()
+postgres_options = "-h localhost -k ./ -p 5488"
 log_file = opath.join(pgdata_path, "initlog.log")
 
 pg_ctl = ("pg_ctl", "--pgdata", pgdata_path, "-o", postgres_options)
@@ -34,8 +34,7 @@ commands = {"initdb": initdb,
 def main():
     global log
 
-    with environment(sys.argv) as env:
-        log = logging.getLogger(__name__)
-        cmd = sys.argv[2]
+    log = logging.getLogger(__name__)
 
-        commands[cmd]()
+    cmd = sys.argv[2]
+    commands[cmd]()
