913b6399bca87f45a8e08b3ebcb7e6e273a1b57d
Stuart Gale <bishboria@gmail.com>
2011-09-09 13:35:33 +0100

    Can run trade history as a background process.

--- londonriots/scripts/trade_history.py
+++ londonriots/scripts/trade_history.py
@@ -3,11 +3,18 @@ import sys
 import londonriots.models as models
 import londonriots.feeds.currency as currency
 import transaction
+import time
 
 def main():
     with environment(sys.argv) as env:
-        currency_pairs = models.DBSession.query(models.CurrencyPair)
-        for currency_pair in currency_pairs:
-            price = currency.CurrencyPriceYahoo(currency_pair)
-            print currency_pair.source, currency_pair.target, price.rate
-        transaction.commit()
+        while True:
+            fetch()
+            time.sleep(30)
+
+def fetch():
+    currency_pairs = models.DBSession.query(models.CurrencyPair)
+    for currency_pair in currency_pairs:
+        price = currency.CurrencyPriceYahoo(currency_pair)
+        print currency_pair.source, currency_pair.target, price.rate, price.effective_date
+    print
+    transaction.commit()
