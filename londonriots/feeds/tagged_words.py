edf5f6877f7ae32d3918acc487bff797d098b633
Stuart Gale <bishboria@gmail.com>
2011-09-12 17:50:24 +0100

    Updating tagged_words to try and clean up the part of speech tagging...

--- londonriots/feeds/tagged_words.py
+++ londonriots/feeds/tagged_words.py
@@ -7,9 +7,6 @@ from sqlalchemy.orm.exc import NoResultFound
 def tag_article(article):
     for word_frequency in article.word_frequencies:
         models.DBSession.delete(word_frequency)
-    article_text = BeautifulSoup(article.source_text)
-    text = u"".join(p.text for p in article_text.findAll("p") if len(p.findAll('select')) == 0)
-    tagged_words = [(w,unicode(p)) for w,p in nltk.pos_tag(nltk.word_tokenize(text)) if w[0].isalpha() and (p[0] in "NV") and (not any(c.isupper() for c in w[1:]))]
     for (word, pos), v in it.groupby(sorted(tagged_words)):
         try:
             tagged_word =  models.DBSession.query(models.TaggedWord).filter(
@@ -18,3 +15,10 @@ def tag_article(article):
         except NoResultFound:
             tagged_word = models.TaggedWord(word, pos)
         models.WordFrequency(article, tagged_word, len(list(v)))
+
+def extract_text(article):
+    article_text = BeautifulSoup(article.source_text)
+    text = u". ".join(p.text for p in article_text.findAll("p") if len(p.findAll('select')) == 0)
+    word_tokenize = nltk.word_tokenize(text)
+    tagged_words = [(w,unicode(p)) for w,p in nltk.pos_tag(word_tokenize)] # if w[0].isalpha() and (p[0] in "NV") and (not any(c.isupper() for c in w[1:]))]
+    return tagged_words
--- londonriots/scripts/tag_article.py
+++ londonriots/scripts/tag_article.py
@@ -7,9 +7,15 @@ import time
 from BeautifulSoup import BeautifulSoup
 import nltk
 import londonriots.feeds.tagged_words as tagged_words
+import pprint as pp
+from html2text import html2text
 
 def main():
     with environment(sys.argv) as env:
-        for article in models.DBSession.query(models.Article):
-            tagged_words.tag_article(article)
-        transaction.commit()
+        for article in models.DBSession.query(models.Article)[:10]:
+            print article.url
+            print html2text(article.source_text.encode("iso-8859-1"))
+            #pp.pprint(tagged_words.extract_text(article))
+            print "="*90
+            #tagged_words.tag_article(article)
+        #transaction.commit()
