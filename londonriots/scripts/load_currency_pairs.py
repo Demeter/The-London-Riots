b68d66d32fa1b8561003076eaf166767a5451c6e
Stuart Gale <bishboria@gmail.com>
2011-09-14 14:20:55 +0100

    Adding more currency pairs.

--- londonriots/scripts/load_currency_pairs.py
+++ londonriots/scripts/load_currency_pairs.py
@@ -3,7 +3,7 @@ import sys
 import londonriots.models as models
 import transaction
 TEST_FEED_URL = "http://news.google.com/news?hl=en&gl=us&q=%(source)s+%(target)s&um=1&ie=UTF-8&output=rss"
-currency_pair_args = (("GBP", "USD"), ("EUR", "USD"))
+currency_pair_args = (("GBP", "USD"), ("EUR", "USD"), ("GBP", "JPY"), ("USD", "JPY"), ("AUD", "USD"))
 
 def main():
     with environment(sys.argv) as env:
