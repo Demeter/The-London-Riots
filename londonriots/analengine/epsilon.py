20d8b959f42842c1806d21b965fd76888aa4d4f4
Stuart Gale <bishboria@gmail.com>
2011-09-15 16:00:20 +0100

    Getting trade rate a given time. Adding currency pair parameter to analengine. Added tests.

--- londonriots/analengine/epsilon.py
+++ londonriots/analengine/epsilon.py
@@ -1,8 +1,21 @@
-from londonriots.models import Article, DBSession, NamedEntity, NamedEntityFrequency
+from londonriots.models import Article, DBSession, NamedEntity, NamedEntityFrequency, TradeRate
+import datetime as dt
 
-def named_entities_in_time_range(start_time, epsilon):
+def named_entities_in_time_range(currency_pair, start_time, epsilon):
     articles = DBSession.query(NamedEntity).join(NamedEntityFrequency).join(Article).filter(
             (Article.effective_date >= start_time) &
             (Article.effective_date <= start_time + epsilon) &
+            (Article.currency_pair == currency_pair) &
             (NamedEntityFrequency.frequency > 1))
     return articles
+
+def currency_price_at_time(currency_pair, time):
+    rates = DBSession.query(TradeRate).filter(
+            (TradeRate.effective_date >= time - dt.timedelta(seconds=60)) &
+            (TradeRate.effective_date <= time + dt.timedelta(seconds=60)) &
+            (TradeRate.currency_pair == currency_pair))
+    n = rates.count()
+    if n == 0: raise KeyError("No trade information available for that time.")
+
+    rate = sum(r.rate for r in rates)/n
+    return rate
--- londonriots/tests/test_analengine.py
+++ londonriots/tests/test_analengine.py
@@ -1,13 +1,24 @@
 from londonriots.tests import TestLR
 import datetime as dt
 import londonriots.analengine.epsilon as anal
+from londonriots.models import root
 
 class TestAnalEngine(TestLR):
     def setUp(self):
         TestLR.setUp(self)
-        self.start_time = dt.datetime(2011, 9, 9, 5)
-        self.epsilon = dt.timedelta(hours=5)
-        self.named_entities = anal.named_entities_in_time_range(self.start_time, self.epsilon)
+        self.currency_pair = root[("GBP", "USD")]
+        self.article_start_time = dt.datetime(2011, 9, 9, 11)
+        self.article_epsilon = dt.timedelta(hours=5)
+        self.named_entities = anal.named_entities_in_time_range(self.currency_pair, self.article_start_time, self.article_epsilon)
+
+        self.currency_price_at_time = self.article_start_time + self.article_epsilon + dt.timedelta(minutes=5)
+        self.currency_price = anal.currency_price_at_time(self.currency_pair, self.currency_price_at_time)
 
     def test_analengine_named_entities(self):
         self.assert_(self.named_entities.count() > 0)
+
+    def test_analengine_currency_price_after_time(self):
+        self.assert_(self.currency_price > 0)
+
+    def test_analengine_has_no_prices(self):
+        self.assertRaises(KeyError, lambda: anal.currency_price_at_time(self.currency_pair, dt.datetime(2001, 9, 9)))
