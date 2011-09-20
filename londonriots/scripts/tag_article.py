3280113f58a768c55e3a79e1d710344f9a58dc8d
Evan Cofsky <evan@tunixman.com>
2011-09-20 09:25:17 +0100

    Made a loop, added transaction/session management.

--- londonriots/scripts/tag_article.py
+++ londonriots/scripts/tag_article.py
@@ -8,14 +8,25 @@ from BeautifulSoup import BeautifulSoup
 import nltk
 import londonriots.feeds.tagged_words as tagged_words
 import pprint as pp
+import logging
+
+log = logging.getLogger(__name__)
 
 def main():
     with environment(sys.argv) as env:
-        for article in models.DBSession.query(models.Article):
-            article = models.DBSession.merge(article)
-            
-            if len(article.entity_frequencies) > 0: continue
+        while True:
+            try:
+                tag_articles()
+                transaction.commit()
+            except:
+                log.error(tb.format_exc())
+                transaction.abort()
 
-            tagged_words.tag_article(article)
-            transaction.commit()
+            models.DBSession.close()
+            time.sleep(30)
 
+def tag_articles():
+    for article in models.DBSession.query(models.Article):
+        if len(article.entity_frequencies) > 0: continue
+
+        tagged_words.tag_article(article)
